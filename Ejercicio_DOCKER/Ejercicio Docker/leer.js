bc542c3014ecc93af55ae93875c5d7a68b006cfc
harunsanduvac38 <harunsanduvac@gmail.com>
2025-05-13 13:36:12 +0200

    modulo_2

--- Ejercicio_DOCKER/Ejercicio Docker/leer.js
+++
@@ -1,7 +0,0 @@
-// Para usarlo se debe instalar primero el paquete readline-sync
-// npm install readline-sync
-const readlineSync = require('readline-sync');
-function leer(promptMessage) {
-    return readlineSync.question(promptMessage);
-}
-module.exports = leer;
